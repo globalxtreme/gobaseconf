11c53bc893116240d96e2122d9630e4c1a693cb8
yuswa-arba <yuswa98@gmail.com>
2023-09-11 17:57:28 +0800

    TASK: Change employee identifier

--- middleware/EmployeeIdentifier.go
+++ middleware/EmployeeIdentifier.go
@@ -1,12 +1,15 @@
 package middleware
 
 import (
+	"crypto/aes"
+	"crypto/cipher"
 	"encoding/base64"
 	"encoding/json"
 	"fmt"
 	"github.com/globalxtreme/gobaseconf/data"
 	"github.com/globalxtreme/gobaseconf/response/error"
 	"net/http"
+	"strings"
 )
 
 func EmployeeIdentifier(next http.Handler) http.Handler {
@@ -16,12 +19,26 @@ func EmployeeIdentifier(next http.Handler) http.Handler {
 			error.ErrUnauthenticated("IDENTIFIER not found")
 		}
 
-		jsonDecode, err := base64.StdEncoding.DecodeString(token)
+		tokenDecode, err := base64.StdEncoding.DecodeString(token)
 		if err != nil {
 			error.ErrUnauthenticated("Unable to decode token!!")
 		}
 
-		err = json.Unmarshal(jsonDecode, &data.Employee)
+		iv := tokenDecode[0:16]
+		tokenExplode := strings.Split(string(tokenDecode[16:]), "-:-")
+
+		secret := []byte(tokenExplode[1][0:tokenDecode[0]])
+		identifierData := []byte(tokenExplode[1][0:])
+
+		block, err := aes.NewCipher(secret)
+		if err != nil {
+			fmt.Println("Failed to initialize cipher: ", err)
+			return
+		}
+		mode := cipher.NewCBCDecrypter(block, iv)
+		mode.CryptBlocks(identifierData, identifierData)
+
+		err = json.Unmarshal(identifierData, &data.Employee)
 		if err != nil {
 			fmt.Println("Error:", err)
 			return
