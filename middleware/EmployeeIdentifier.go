dede175b6a178b3c7ca9a5d90be587a416637a80
yuswa-arba <yuswa98@gmail.com>
2023-09-12 15:02:11 +0800

    Change employee identifier

--- middleware/EmployeeIdentifier.go
+++ middleware/EmployeeIdentifier.go
@@ -1,14 +1,18 @@
 package middleware
 
 import (
+	"bytes"
+	"compress/gzip"
 	"crypto/aes"
 	"crypto/cipher"
 	"encoding/base64"
 	"encoding/json"
 	"fmt"
 	"github.com/globalxtreme/gobaseconf/data"
-	"github.com/globalxtreme/gobaseconf/response/error"
+	errResponse "github.com/globalxtreme/gobaseconf/response/error"
+	"io"
 	"net/http"
+	"strconv"
 	"strings"
 )
 
@@ -16,34 +20,66 @@ func EmployeeIdentifier(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		token := r.Header.Get("IDENTIFIER")
 		if len(token) == 0 {
-			error.ErrUnauthenticated("IDENTIFIER not found")
+			errResponse.ErrUnauthenticated("IDENTIFIER not found")
 		}
 
 		tokenDecode, err := base64.StdEncoding.DecodeString(token)
 		if err != nil {
-			error.ErrUnauthenticated("Unable to decode token!!")
+			errResponse.ErrUnauthenticated("Unable to decode token!!")
 		}
 
 		iv := tokenDecode[0:16]
 		tokenExplode := strings.Split(string(tokenDecode[16:]), "-:-")
 
-		secret := []byte(tokenExplode[1][0:tokenDecode[0]])
-		identifierData := []byte(tokenExplode[1][0:])
+		secretLength, _ := strconv.Atoi(tokenExplode[0])
+		secret := []byte(tokenExplode[1][0:secretLength])
+		identifierData := []byte(tokenExplode[1][secretLength:])
 
 		block, err := aes.NewCipher(secret)
 		if err != nil {
-			fmt.Println("Failed to initialize cipher: ", err)
-			return
+			errResponse.ErrUnauthenticated(fmt.Sprintf("Unable to decode token!! %s", err))
 		}
+
 		mode := cipher.NewCBCDecrypter(block, iv)
 		mode.CryptBlocks(identifierData, identifierData)
+		identifierData = unpadPKCS7(identifierData)
+
+		identifierData, err = DecompressZlib(identifierData)
+		if err != nil {
+			errResponse.ErrUnauthenticated(fmt.Sprintf("Unable to decompress data: %s", err))
+		}
 
 		err = json.Unmarshal(identifierData, &data.Employee)
 		if err != nil {
-			fmt.Println("Error:", err)
-			return
+			errResponse.ErrUnauthenticated(fmt.Sprintf("Unable to decode data json: %s", err))
 		}
 
 		next.ServeHTTP(w, r)
 	})
+}
+
+func unpadPKCS7(data []byte) []byte {
+	length := len(data)
+	unpadding := int(data[length-1])
+	return data[:(length - unpadding)]
+}
+
+func DecompressZlib(data []byte) (resData []byte, err error) {
+	b := bytes.NewBuffer(data)
+
+	var r io.Reader
+	r, err = gzip.NewReader(b)
+	if err != nil {
+		return
+	}
+
+	var resB bytes.Buffer
+	_, err = resB.ReadFrom(r)
+	if err != nil {
+		return
+	}
+
+	resData = resB.Bytes()
+
+	return
 }
