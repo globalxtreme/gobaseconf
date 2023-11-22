0a9be9453f4371248439be7acd4efc02d3c11c7e
yuswa-arba <yuswa98@gmail.com>
2023-11-22 12:16:51 +0800

    TASK: Show panic err in migration

--- database/migration/migrate.go
+++ database/migration/migrate.go
@@ -3,9 +3,11 @@ package migration
 import (
 	"github.com/globalxtreme/gobaseconf/config"
 	"gorm.io/gorm"
+	"log"
 )
 
 func Migrate(tables []Table, columns []Column) {
+	var err error
 	var migration *gorm.DB
 	var migrator gorm.Migrator
 
@@ -19,21 +21,30 @@ func Migrate(tables []Table, columns []Column) {
 		if table.CreateTable != nil {
 			migrator = migration.Table(table.CreateTable.TableName()).Migrator()
 			if !migrator.HasTable(table.CreateTable) {
-				migrator.CreateTable(table.CreateTable)
+				err = migrator.CreateTable(table.CreateTable)
+				if err != nil {
+					log.Panicf("CREATE CREATE: %v", err)
+				}
 			}
 		}
 
 		if len(table.RenameTable.Old) > 0 {
 			migrator = migration.Table(table.RenameTable.Old).Migrator()
 			if migrator.HasTable(table.RenameTable.Old) {
-				migrator.RenameTable(table.RenameTable.Old, table.RenameTable.New)
+				err = migrator.RenameTable(table.RenameTable.Old, table.RenameTable.New)
+				if err != nil {
+					log.Panicf("RENAME TABLE: %v", err)
+				}
 			}
 		}
 
 		if len(table.DropTable) > 0 {
 			migrator = migration.Table(table.DropTable).Migrator()
 			if migrator.HasTable(table.DropTable) {
-				migrator.DropTable(table.DropTable)
+				err = migrator.DropTable(table.DropTable)
+				if err != nil {
+					log.Panicf("DROP TABLE: %v", err)
+				}
 			}
 		}
 	}
@@ -50,7 +61,10 @@ func Migrate(tables []Table, columns []Column) {
 		if len(column.RenameColumns) > 0 {
 			for _, rename := range column.RenameColumns {
 				if migrator.HasColumn(column.Model, rename.Old) {
-					migrator.RenameColumn(column.Model, rename.Old, rename.New)
+					err = migrator.RenameColumn(column.Model, rename.Old, rename.New)
+					if err != nil {
+						log.Panicf("RENAME COLUMN: %v", err)
+					}
 				}
 			}
 		}
@@ -58,7 +72,10 @@ func Migrate(tables []Table, columns []Column) {
 		if len(column.AddColumns) > 0 {
 			for _, add := range column.AddColumns {
 				if !migrator.HasColumn(column.Model, add) {
-					migrator.AddColumn(column.Model, add)
+					err = migrator.AddColumn(column.Model, add)
+					if err != nil {
+						log.Panicf("ADD COLUMN: %v", err)
+					}
 				}
 			}
 		}
@@ -66,7 +83,10 @@ func Migrate(tables []Table, columns []Column) {
 		if len(column.DropColumns) > 0 {
 			for _, drop := range column.DropColumns {
 				if migrator.HasColumn(column.Model, drop) {
-					migrator.DropColumn(column.Model, drop)
+					err = migrator.DropColumn(column.Model, drop)
+					if err != nil {
+						log.Panicf("DROP COLUMN: %v", err)
+					}
 				}
 			}
 		}
@@ -74,7 +94,10 @@ func Migrate(tables []Table, columns []Column) {
 		if len(column.AlterColumns) > 0 {
 			for _, alter := range column.AlterColumns {
 				if migrator.HasColumn(column.Model, alter) {
-					migrator.AlterColumn(column.Model, alter)
+					err = migrator.AlterColumn(column.Model, alter)
+					if err != nil {
+						log.Panicf("ALTER COLUMN: %v", err)
+					}
 				}
 			}
 		}
